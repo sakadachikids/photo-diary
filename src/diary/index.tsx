25438b33c9d5bedd6c52488d08fb0f21999243f1
Ryota.Kunisada <r.kunisada661@gmail.com>
2019-05-14 09:18:00 +0900

    add firestore

--- src/diary/index.tsx
+++ src/diary/index.tsx
@@ -1,10 +1,23 @@
 import React from "react";
 import createUseContext from "constate";
+import getDbInstance from "../getDbInstance";
 
 const useDiary = () => {
   const [diaries, setDiaries] = React.useState<Diary[]>([]);
+
+  const db = getDbInstance();
+
+  React.useEffect(() => {
+    (async () => {
+      const collections = await db.collection("diaries").get();
+      collections.forEach(doc => {
+        setDiaries(prevState => [...prevState, doc.data() as Diary]);
+      });
+    })();
+  }, [db]);
+
   const addDiary = (diary: Diary) => {
-    // TODO: サーバーにデータを投げる
+    db.collection("diaries").add(diary);
     setDiaries(prevState => [...prevState, diary]);
   };
   return { diaries, addDiary };
