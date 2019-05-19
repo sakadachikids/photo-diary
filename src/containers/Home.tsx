6f00d1471339f043e7b9829ea0c24108c1aeac47
92thunder <r.kunisada661@gmail.com>
2019-05-19 14:58:40 +0900

    サインイン画面実装

--- src/containers/Home.tsx
+++ src/containers/Home.tsx
@@ -21,53 +21,8 @@ export default () => {
           // ...
         }
         // The signed-in user info.
-        var user = result.user;
+        setUser(result.user);
       });
-    firebase.auth().onAuthStateChanged(
-      function(user) {
-        console.log(user);
-        if (user) {
-          setUser(user);
-          // User is signed in.
-          var displayName = user.displayName;
-          var email = user.email;
-          var emailVerified = user.emailVerified;
-          var photoURL = user.photoURL;
-          var uid = user.uid;
-          var phoneNumber = user.phoneNumber;
-          var providerData = user.providerData;
-          user.getIdToken().then(function(accessToken) {
-            document.getElementById("sign-in-status")!.textContent =
-              "Signed in";
-            document.getElementById("sign-in")!.textContent = "Sign out";
-            document.getElementById(
-              "account-details"
-            )!.textContent = JSON.stringify(
-              {
-                displayName: displayName,
-                email: email,
-                emailVerified: emailVerified,
-                phoneNumber: phoneNumber,
-                photoURL: photoURL,
-                uid: uid,
-                accessToken: accessToken,
-                providerData: providerData
-              },
-              null,
-              "  "
-            );
-          });
-        } else {
-          // User is signed out.
-          document.getElementById("sign-in-status")!.textContent = "Signed out";
-          document.getElementById("sign-in")!.textContent = "Sign in";
-          document.getElementById("account-details")!.textContent = "null";
-        }
-      },
-      function(error) {
-        console.log(error);
-      }
-    );
   };
   React.useEffect(() => {
     initApp();
@@ -86,9 +41,6 @@ export default () => {
           return <SignIn />;
         }
       })()}
-      <div id="sign-in-status" />
-      <div id="sign-in" />
-      <pre id="account-details" />
     </React.Fragment>
   );
 };
--- src/containers/SignIn.tsx
+++ src/containers/SignIn.tsx
@@ -1,37 +1,63 @@
 import firebase from "firebase/app";
-import firebaseui from "firebaseui";
 import "firebaseui/dist/firebaseui.css";
 import React from "react";
+import * as Mat from "@material-ui/core";
+import {
+  createStyles,
+  withStyles,
+  Theme,
+  WithStyles
+} from "@material-ui/core/styles";
 
-export default () => {
-  React.useEffect(() => {
-    let ui = firebaseui.auth.AuthUI.getInstance();
-    if (!ui) {
-      ui = new firebaseui.auth.AuthUI(firebase.auth());
+const styles = (theme: Theme) =>
+  createStyles({
+    container: {
+      height: "100vh",
+      backgroundColor: theme.palette.primary.main,
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center"
+    },
+    title: {
+      height: "60%",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      color: "white"
+    },
+    signin: {
+      height: "40%",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center"
     }
-    const uiConfig = {
-      callbacks: {
-        signInSuccessWithAuthResult: (authResult: any, redirectUrl: string) => {
-          console.log(authResult);
-          console.log(redirectUrl);
-          return true;
-        },
-        uiShown: () => {
-          document.getElementById("loader")!.style.display = "none";
-        }
-      },
-      signInSuccessUrl: "/",
-      signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
-      tosUrl: "terms",
-      privacyPolicyUrl: "policy"
-    };
-    ui.start("#firebaseui-auth-container", uiConfig);
-  }, []);
+  });
+
+type Props = WithStyles<typeof styles>;
+
+const SignIn = ({ classes }: Props) => {
+  const signIn = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase
+      .auth()
+      .signInWithRedirect(provider)
+      .then(function(result) {
+        console.log(result);
+      });
+  };
 
   return (
-    <div>
-      <div id="firebaseui-auth-container" />
-      <div id="loader">Now Loading...</div>
+    <div className={classes.container}>
+      <div className={classes.title}>
+        <h1>Photo Diary</h1>
+      </div>
+      <div className={classes.signin}>
+        <Mat.Button onClick={signIn} color="secondary" variant="contained">
+          ログインする
+        </Mat.Button>
+      </div>
     </div>
   );
 };
+
+export default withStyles(styles)(SignIn);
