b4d109ab715f77d6c5526f54d053cdcab37705e7
taroodr <n.kotaro.0101@gmail.com>
2019-05-19 14:10:20 +0900

    localからscriptで通知が打てるようにした

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,43 @@
+/*
+   JavaScript クライアントでメッセージを受信する  |  Firebase
+   https://firebase.google.com/docs/cloud-messaging/js/receive?hl=ja
+ */
+
+// Give the service worker access to Firebase Messaging.
+// Note that you can only use Firebase Messaging here, other Firebase libraries
+// are not available in the service worker.
+importScripts("https://www.gstatic.com/firebasejs/5.7.3/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/5.7.3/firebase-messaging.js");
+
+firebase.initializeApp({
+  messagingSenderId: "320008978807"
+});
+
+// バックグラウンドメッセージを処理できるようにFirebase Messagingのインスタンスを取得します。
+// この一文がないとブラウザがアクティブなときでもPush通知を受信できません。
+const messaging = firebase.messaging();
+
+// サーバー側にPOSTするデータに `notification` が含まれている場合は
+// `setBackgroundMessageHandler` は呼ばれない。
+// `notification` をサーバー側で設定しない場合はここで通知のtitle, bodyを組み立てることになる。
+// => Firebase Cloud Messaging（FCM）でより簡単にWebブラウザにPush通知を送るサンプル - DRYな備忘録
+//    http://otiai10.hatenablog.com/entry/2017/06/22/023025
+messaging.setBackgroundMessageHandler(payload => {
+  console.log(
+    "[firebase-messaging-sw.js] Received background message ",
+    payload
+  );
+  // Customize notification here
+  const notificationTitle = "Background Message Title";
+  const notificationOptions = {
+    title: "push-test-web",
+    body: "push-test-web: Background Message body",
+    icon: "/favicon.ico"
+    // icon: '/firebase-logo.png',
+  };
+
+  return self.registration.showNotification(
+    notificationTitle,
+    notificationOptions
+  );
+});
--- src/getDbInstance.ts
+++ src/getDbInstance.ts
@@ -4,11 +4,27 @@ import "firebase/firestore";
 firebase.initializeApp({
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID
 });
 
 const getDbInstance = () => {
   return firebase.firestore();
 };
+
+const initializeFCM = async () => {
+  const messaging = firebase.messaging();
+
+  // const token = await messaging.getToken();
+  messaging.usePublicVapidKey(
+    "BI28glpT6KKmgZf0EI6eOq9MPDfmyqOGSTMQS3gQUtq_Loxc-VN07MTgERl6GIW2UZgkrGxxwMqvt5kLtF6kY0A"
+  );
+
+  await messaging.requestPermission();
+  // const token = await messaging.getToken();
+  // console.log(token);
+};
+
+initializeFCM();
 
 export default getDbInstance;
